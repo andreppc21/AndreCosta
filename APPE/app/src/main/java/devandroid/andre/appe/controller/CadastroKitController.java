0e40ed035ca8a0b8077c5042b4f5e657d0eeebbd
andreppc21 <andreppc21@gmail.com>
2024-04-11 10:58:52 -0300

    implementação da controller MVC

--- APPE/app/src/main/java/devandroid/andre/appe/controller/CadastroKitController.java
+++ APPE/app/src/main/java/devandroid/andre/appe/controller/CadastroKitController.java
@@ -1,7 +1,27 @@
 package devandroid.andre.appe.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.andre.appe.model.CadastroKit;
+
 public class CadastroKitController {
 
+   public void salvar (CadastroKit cadastroKit){
+       Log.i("MVC_Controller","salvo"+ cadastroKit.toString());
 
 
+   }
+
+
+    @NonNull
+    @Override
+    public String toString() {
+
+
+        Log.i("MVC_Controller","controller iniciada");
+
+        return super.toString();
+    }
 }
