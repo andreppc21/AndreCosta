82d41631f17288142644ec3e41417c3180171436
andreppc21 <andreppc21@gmail.com>
2024-04-17 11:10:02 -0300

    criado o adapter para popular o spinner

--- APPE/app/src/main/java/devandroid/andre/appe/view/MainActivity.java
+++ APPE/app/src/main/java/devandroid/andre/appe/view/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
         btnLimpar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
